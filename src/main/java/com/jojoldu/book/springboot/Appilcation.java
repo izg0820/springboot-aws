76b214631a44b82eabbef05dfc11dfcf276ced20
wincrane_2 <izg0820@naver.com>
2020-09-21 22:15:48 +0900

    2020.09.21 commmit

---
+++ src/main/java/com/jojoldu/book/springboot/Appilcation.java
@@ -0,0 +1,11 @@
+package com.jojoldu.book.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Appilcation {
+    public static void main(String[] args) {
+        SpringApplication.run(Appilcation.class,args);
+    }
+}
